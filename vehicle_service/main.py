c10a10b07adb3e83f552f6756e86d6cc0b084249
RanWeissman <ranw535@gmail.com>
2026-06-08 16:33:34 +0300

    vehcile_service_implementation

--- vehicle_service/main.py
+++ vehicle_service/main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from vehicle-service!")
+from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
+from routers import cars
+from logger import get_logger
 
-if __name__ == "__main__":
-    main()
+logger = get_logger("main")
+
+app = FastAPI(title="Vehicle Service API")
+
+app.include_router(cars.router)
+
+instrumentator = Instrumentator().instrument(app).expose(app)
+
+@app.on_event("startup")
+async def startup_event():
+    logger.info("Vehicle Service is starting up...")
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    logger.info("Vehicle Service is shutting down...")
